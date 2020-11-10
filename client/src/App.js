d3090c8e6d3a41a2053861ccbfac2089e67954b5
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-10 14:15:02 -0500

    import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
+import AddProduct from './components/AddProduct';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import ProductList from './components/ProductList';
 
-    </div>
-  );
+import Context from './Context.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: null,
+      cart: {},
+      products: [],
+    };
+
+    this.routerRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          login: this.login,
+          addToCart: this.addToCart,
+          addProduct: this.addProduct,
+          removeFromCart: this.removeFromCart,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/products" component={ProductList} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/cart" component={Cart} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
 }
 
 export default App;
